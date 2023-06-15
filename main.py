a664d3ff6c5ebad75ad9153cb229145eeb3594b9
jaehyunjang <hyuni2020@gmail.com>
2023-06-15 16:52:05 +0900

    add log

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ from rembg import remove
 from PIL import Image
 import numpy as np
 import io
+import logging
 
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
 @app.post("/removebg")
 async def remove_background(file: UploadFile = File(...)):
-    contents = await file.read()
-    img = Image.open(io.BytesIO(contents))
-    img = np.array(img)
-    result = remove(img)
-    img = Image.fromarray(result)
-    byte_arr = io.BytesIO()
-    img.save(byte_arr, format="PNG")
-    byte_arr = byte_arr.getvalue()
-    return Response(content=byte_arr, media_type="image/png")
+    try:
+        contents = await file.read()
+        img = Image.open(io.BytesIO(contents))
+        img = np.array(img)
+        result = remove(img)
+        img = Image.fromarray(result)
+        byte_arr = io.BytesIO()
+        img.save(byte_arr, format="PNG")
+        byte_arr = byte_arr.getvalue()
+        return Response(content=byte_arr, media_type="image/png")
+    except Exception as e:
+        logger.error(e)
+        raise
